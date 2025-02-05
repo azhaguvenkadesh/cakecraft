718850c9aa8abfd8db2ec8996b0a80a0527b8ff8
azhaguvenkadesh <119848180+azhaguvenkadesh@users.noreply.github.com>
2025-02-05 15:53:24 +0530

    Update apiConfig.js

--- reactapp/src/apiConfig.js
+++ reactapp/src/apiConfig.js
@@ -1,4 +1,4 @@
 
-const API_BASE_URL = 'https://8080-bbefdebbdfedacd318961299cdabfcfbdedbadfcone.premiumproject.examly.io';
+const API_BASE_URL = 'https://448wi22e2m.execute-api.us-east-1.amazonaws.com/final';
 
 export default API_BASE_URL;
